cc2dfc933fc5d0a6ca2174966419f4a8ba0e43c3
jiazhiguo <jiazg@1391.com>
2023-05-09 23:17:24 +0800

    upload

---
+++ webcast/app/src/main/java/com/hxdl/coceweb/MyApplication.java
@@ -0,0 +1,11 @@
+package com.hxdl.coceweb;
+
+import com.hxdl.coceweb.base.BaseApplication;
+
+public class MyApplication extends BaseApplication {
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+    }
+}
